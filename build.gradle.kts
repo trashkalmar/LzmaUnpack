5414e99916f0822c3181ecd509e504d140d37edc
trashkalmar <trashkalmar@gmail.com>
2021-05-15 12:32:05 +0300

    fix: Updated dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
-    maven
-    `maven-publish`
+  kotlin("jvm") version "1.5.0"
+  `maven-publish`
+}
+
+allprojects {
+  repositories {
+    mavenCentral()
+  }
 }
 
 group = "com.github.trashkalmar"
-version = "1.0"
-
-repositories {
-    jcenter()
-    maven("https://jitpack.io")
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-}
+version = "1.0.1"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "unlzma"
+include(":lib")
